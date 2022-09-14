74a2f55b4f815d49b15a5326d34ad80b54155574
gorden <limguancheng@hotmail.com>
2022-09-14 12:38:37 +0800

    change account structure

---
+++ account/src/controllers/v1.controllers.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+
+export const createData = (req: Request, res: Response) => {
+  try {
+    const body = req.body
+    res.status(200).send({ message: 'createData', status: 'success', data: body });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ message: error.message, status: 'error', data: error });
+  }
+};
+
+export const getData = (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    res.status(200).send({ message: 'getData', status: 'success', data: id });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ message: error.message, status: 'error', data: error });
+  }
+};
+
+export const listData = (req: Request, res: Response) => {
+  try {
+    const body = req.body
+    res.status(200).send({ message: 'listData', status: 'success', data: body });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ message: error.message, status: 'error', data: error });
+  }
+};
+
+export const updateData = (req: Request, res: Response) => {
+  try {
+    const body = req.body
+    res.status(200).send({ message: 'updateData', status: 'success', data: body });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ message: error.message, status: 'error', data: error });
+  }
+};
+
+export const deleteData = (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    res.status(200).send({ message: 'deleteData', status: 'success', data: id });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send({ message: error.message, status: 'error', data: error });
+  }
+};
--- account/src/middlewares/authenticate.ts
+++ account/src/middlewares/authenticate.ts
@@ -1,16 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 export const authenticate = () => async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: 'No authorization header', status: 'error' });
+    return;
+  }
+  const [authType, creds] = authHeader.split(' ');
+  if (authType.toLowerCase() !== 'bearer') {
+    res.status(401).send({ message: 'Invalid authorization type', status: 'error' });
+    return;
+  }
   try {
-    if (!authHeader) {
-      throw new Error('No authorization header');
-    }
-    const [authType, creds] = authHeader.split(' ');
-    if (authType.toLowerCase() !== 'bearer') {
-      throw new Error('Invalid authorization type');
-    }
-      
+    // const decodedToken = jwt.verify(creds, process.env.JWT_SECRET!);
     next();
   } catch (error: any) {
     console.error(error);
